3e1d1860c940753cd445db756b4c9bc676771849
Kendek <96197296+Kendek@users.noreply.github.com>
2023-11-09 10:07:22 +0100

    Hiányos fájl beolvasás

---
+++ C#_alkalmazás/Methods.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PC_alkatrészek
+{
+    internal class Methods
+    {
+
+        static List<Read> products;
+
+        static void ReadIn(string fajlnev)
+        {
+            products = new List<Read>();
+
+            var all = File.ReadAllLines(fajlnev);
+
+            string type;
+            string name;
+            string parameters;
+            int price;
+
+            foreach (var line in all)
+            {
+                var line_parts = line.Trim(';').Split(";");
+                type = line_parts[0];
+                name = line_parts[1];
+                parameters = line_parts[2];
+                price = Convert.ToInt32(line_parts[3]);
+
+
+                products.Add(new Read(type, name, parameters, price));
+            }
+        }
+
+    }
+}
---
+++ C#_alkalmazás/Read.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PC_alkatrészek
+{
+    internal class Read
+    {
+        /*
+         * type
+         * name
+         * parameters:
+         *      -CPU --> frequency, socket, cores
+         *      -Motherboard --> cpu socket
+         *      -RAM --> frequency, size
+         *      -GPU --> frequency, memory size
+         *      -HDD/SSD --> speed, size
+         *      -Monitor --> resolution, Hz
+         *      -Mouse --> DPI, vire-less
+         *      -Keyboard --> mechanic/membranous
+         * price(Ft)
+        */
+
+        string type;
+        string name;
+        string parameters;
+        int price;
+
+
+        public Read(string type, string name, string parameters, int price)
+        {
+            this.type = type;
+            this.name = name;
+            this.parameters = parameters;
+            this.price = price;
+        }
+    }
+}
